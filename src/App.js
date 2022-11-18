88fc9e41847167ab8a53ef2a8934dafe6003cb5e
Jose Trujillo <joseantoniotsandoval@gmail.com>
2022-11-18 07:22:25 -0300

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,46 @@
-import logo from './logo.svg';
+import React, { useEffect } from 'react'
+import { useDispatch } from 'react-redux';
 import './App.css';
+import Acordeon from './components/Acordeon';
+import { CircunferenciaAbdominal } from './content/CircunferenciaAbdominal';
+import { GlicemiaAyuna } from './content/GlicemiaAyuna';
+import { Imc } from './content/Imc';
+import { Lipidos } from './content/Lipidos';
+import { PresionArterial } from './content/PresionArterial';
+import { getExams } from './store/history/thunks';
 
 function App() {
+
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getExams())
+  }, []);
+
+
+  const data = [
+    { id: 1, target: "#collapseOne", heading: "headingOne", controls: "collapseOne", name: "Perfil Lipídico", content: () => <Lipidos /> },
+    { id: 2, target: "#collapseTwo", heading: "headingTwo", controls: "collapseTwo", name: "Presion arterial", content: () => <PresionArterial /> },
+    { id: 3, target: "#collapseThree", heading: "headingThree", controls: "collapseThree", name: "Glicemia en ayunas", content: () => <GlicemiaAyuna /> },
+    { id: 4, target: "#collapseFour", heading: "headingFour", controls: "collapseFour", name: "Imc", content: () => <Imc /> },
+    { id: 5, target: "#collapseFive", heading: "headingFive", controls: "collapseFive", name: "Circunferencia Abdominal", content: () => <CircunferenciaAbdominal /> }
+  ];
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+    
+      <div className='container mt-5  '>
+        <div className="row">
+          <div className='col'>
+            {/* {data.map((item, index) => (
+                <Acordeon key={index} name={item.name} content={item.content} heading={item.heading} target={item.target} controls={item.controls} />))} */}
+            <Acordeon data={data} />
+          </div>
+        </div>
+      </div>
+
+
+
   );
 }
 
